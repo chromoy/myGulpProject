1e3a6a8c8260e2870863cf04d4e73110b8fd9f2f
chromoy <chromoy000@gmail.com>
2022-06-27 09:07:44 +0300

    first version

---
+++ src/scripts/classes.js
@@ -0,0 +1,13 @@
+class Task {
+    constructor() {
+        console.log("Создан экземпляр task!");
+    }
+
+    showId() {
+        console.log(23);
+    }
+
+    static loadAll() {
+        console.log("Загружаем все tasks...");
+    }
+}
